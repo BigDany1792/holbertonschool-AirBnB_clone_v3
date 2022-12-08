91448511b457434fde14dfc2cde9a1eb9372a4db
WilsonValer <nederig21@gmail.com>
2022-12-08 17:48:32 -0500

    modified all task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-''' Return the status of an API '''
-from flask import Flask
-from models import storage
+""" Method that starts a Flask web application """
 from api.v1.views import app_views
-from os import getenv  # to use environmental variables
+from flask import Flask
 from flask import jsonify
-from werkzeug.exceptions import HTTPException  # to use errorhandler
+from flask import make_response
+from models import storage
+import os
 
-
-# instance app variable from Flask class
 app = Flask(__name__)
-# register the blueprint app_views for use
+
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    ''' Remove the current SQLAlchemy Session '''
+def close(self):
+    """ this method logs out the database session """
     storage.close()
 
 
-@app.errorhandler(HTTPException)
-def handle_exception(error):
-    ''' Use errorhandler to display 404 error page '''
-    return jsonify({"error": "Not found"}), 404
+@app.errorhandler(404)
+def not_found(error):
+    """ Error 404 """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == '__main__':
-    # return env variable if it exists
-    # otherwise return second argument
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True, debug=True)
+if __name__ == "__main__":
+    app.run(
+            host=os.getenv("HBNB_API_HOST", '0.0.0.0'),
+            port=os.getenv("HBNB_API_PORT", 5000),
+            threaded=True,
+            debug=True
+           )
+# app.config["DEBUG"] = True
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""This module implement a rule that returns the status of the application"""
+"""
+Module index:
+   url_prefix = '/api/v1'
+   routes:
+       - '/status'
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-@app_views.route("/status", strict_slashes=False)
-def view_status():
-    """View function that return a json message"""
+
+@app_views.route('/status')
+def status():
+    """ Method status """
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def view():
-    """retrieves the number of each objects by type:"""
-    return {
-            "amenities": storage.count('Amenity'),
-            "cities": storage.count('City'),
-            "places": storage.count('Place'),
-            "reviews": storage.count('Review'),
-            "states": storage.count('State'),
-            "users": storage.count('User')
-            }
+@app_views.route('/stats')
+def stats():
+    """ Method stast """
+    return jsonify(
+        {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        }
+    )
