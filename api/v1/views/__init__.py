00664890217ff19bdf177cb1d0eb10184f5054bd
WilsonValer <nederig21@gmail.com>
2022-12-09 15:29:50 -0500

    was added the code task8 import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,9 @@ Module __init__
 """
 from flask import Blueprint
 
+#  Create a Blueprint object called app_views
 app_views = Blueprint('app_views',__name__,url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
