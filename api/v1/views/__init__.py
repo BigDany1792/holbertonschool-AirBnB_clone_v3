6cfeafcd575c9172d677d7877fc2c6baed7eb827
BigDany1792 <dany1792dota@gmail.com>
2022-12-09 22:17:41 -0500

    DOCS: pycodestyle was corrected!

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,6 @@ Module __init__
     instance: app_views
 """
 from flask import Blueprint
-
-#  Create a Blueprint object called app_views
-app_views = Blueprint('app_views',__name__,url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -15,3 +11,5 @@ from api.v1.views.amenities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 
+#  Create a Blueprint object called app_views
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 #  Add views to app_views  using the route decorator
 @app_views.route('/status')
 def status():
